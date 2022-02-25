f08d7487fd0bb2c29ca202b03f3c9d2ed9d8bba0
TENMAJKL <tenmajkl@liskyjidlokod.fun>
2022-02-25 20:05:42 +0100

    implements working lexer

--- src/main.rs
+++ src/main.rs
@@ -3,61 +3,95 @@
 
 use std::process::exit;
 
+#[derive(Debug)]
 enum TokenKind {
     FUNCTION,
-
+    STRING,
+    INT
 }
 
 struct Token {
     kind: TokenKind,
     context: String,
-    body: Vec<Token>
 }
 
-struct Word {
-    content: String,
-    pos: usize,
-    line: usize
-}
 
-fn error(message: String, pos: usize) {
+
+fn error(message: &str, line: usize, pos: usize) {
     println!("ERROR PICO VOLE????: {} NA POZICI VOLE {}.", message, pos);
     exit(-1);
 }
 
-fn lex(program: &String) {
-    let mut result: Vec<Word> = Vec::new();
+fn is_numeric(target: &str) -> bool {
+    for character in target.chars() {
+        if character < '0' || character > '9' {
+            return false;
+        }
+    }
+    return true;
+}
+
+fn tokenize(word: &str) -> Token {
+    let mut kind;
+    if is_numeric(&word) {
+        kind = TokenKind::INT;
+    } else if word.starts_with('"') {
+        kind = TokenKind::STRING;
+    } else {
+        kind = TokenKind::FUNCTION;
+    }
+
+    Token {
+        kind,
+        context: String::from(word)
+    }
+}
+
+fn lex(program: &String) -> Vec<Token> {
+    let mut result: Vec<Token> = Vec::new();
     let mut last = String::new();
     let mut line = 1;
     let mut pos = 0;
     let mut parsing_string = false; 
+    let mut parsing_fn = false;
     for character in program.chars() {
         pos += 1;
         match character {
-            '[' => result.push(Word { content: String::from("["), pos, line }),
-            ']' => { 
-                result.push(Word { content: last.clone(), pos, line });
-                result.push(Word { content: String::from("]"), pos, line });
+            '[' => parsing_fn = true,
+            ']' => {
+                if !last.is_empty() {
+                    result.push(tokenize(&last));
+                }
+                last.clear();
+                parsing_fn = false;
             },
             ' ' =>  {
                 if parsing_string {
                     last.push(' ');
                 } else if !last.is_empty() {
-                    result.push(Word { content: last, pos, line });
+                    result.push(tokenize(&last));
+                    last.clear();
                 }
             },
             '\n' => {
                 pos = 0;
                 line += 1;
             },
-            '"' => parsing_string = !parsing_string,
+            '"' => {
+                last.push('"');
+                parsing_string = !parsing_string;
+            },
             _ => last.push(character)
         }
     }
+    return result;
 }
 
 fn main() {
-    println!("Hello, world!");
+    for i in lex(&String::from("  
+[     print \"parek\"]       [+ 1 [- 1 2]]")) {
+        println!("{:?} {}", i.kind, i.context);
+    }
 }
 
 // [
